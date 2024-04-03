9d551e906dc34a7cdc67229d346d8c08e3f7d60c
sashank45678 <165925811+sashank45678@users.noreply.github.com>
2024-04-03 21:23:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "AIzaSyCYHIA4udpcEn_s8peQqd6wX6KqR0qLZ7M",
-  authDomain: "ngpt-43f57.firebaseapp.com",
-  projectId: "ngpt-43f57",
-  storageBucket: "ngpt-43f57.appspot.com",
-  messagingSenderId: "494797012890",
-  appId: "1:494797012890:web:ba6a7af426ffee94e08423",
-  measurementId: "G-6QPWYKVDTB"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 firebase.initializeApp(firebaseConfig);
 const root = ReactDOM.createRoot(document.getElementById('root'));
